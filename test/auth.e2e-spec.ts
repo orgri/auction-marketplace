9add43a3bb0b341b20ac7500665c918bd7ca5e2b
Vladyslav Reva <juve1991@gmail.com>
2020-12-17 14:53:46 +0200

    test: e2e auth controller

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import { JwtService } from '@nestjs/jwt';
 import * as request from 'supertest';
 
 const user = {
@@ -10,11 +11,12 @@ const user = {
   phone: '+380950000000',
   birth: '1999-01-01',
 };
-
 const password = 'password';
+const newPassword = 'e2epassword';
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
+  let accessToken: string;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -24,6 +26,8 @@ describe('AuthController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+
+    accessToken = app.get<JwtService>(JwtService).sign({ email: user.email });
   });
 
   afterAll(async () => {
@@ -44,7 +48,7 @@ describe('AuthController (e2e)', () => {
       });
     });
 
-    it('should return validation error because of birth constarint', () => {
+    it('should return validation error because of birth constraint', () => {
       return request(app.getHttpServer())
         .post('/auth/signup')
         .send({ ...user, password, birth: '2020-01-01' })
@@ -55,7 +59,7 @@ describe('AuthController (e2e)', () => {
         });
     });
 
-    it('should return validation error because of missing prequired data', () => {
+    it('should return validation error because of missing required data', () => {
       return request(app.getHttpServer())
         .post('/auth/signup')
         .send({})
@@ -78,7 +82,7 @@ describe('AuthController (e2e)', () => {
         });
     });
 
-    it('should return validation error because of unique email constarint', () => {
+    it('should return validation error because of unique email constraint', () => {
       return request(app.getHttpServer())
         .post('/auth/signup')
         .send({ ...user, password })
@@ -89,7 +93,7 @@ describe('AuthController (e2e)', () => {
         });
     });
 
-    it('should return validation error because of unique phone constarint', () => {
+    it('should return validation error because of unique phone constraint', () => {
       return request(app.getHttpServer())
         .post('/auth/signup')
         .send({ ...user, password, email: 'differ_user@example.com' })
@@ -97,6 +101,120 @@ describe('AuthController (e2e)', () => {
         .expect({
           name: 'Validation Error',
           errors: ['You are not able to register'],
+        });
+    });
+  });
+
+  describe('/auth/login POST', () => {
+    it('should not login with incorect password ', async () => {
+      return await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: user.email, password: 'incorrect' })
+        .expect(422)
+        .expect({
+          statusCode: 422,
+          message: 'Wrong email or password',
+          error: 'Unprocessable Entity',
+        });
+    });
+
+    it('should not login with incorect email ', async () => {
+      return await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: 'incorrect@email.com', password })
+        .expect(422)
+        .expect({
+          statusCode: 422,
+          message: 'Wrong email or password',
+          error: 'Unprocessable Entity',
+        });
+    });
+
+    it('should login and return user data with token', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: user.email, password })
+        .expect(200);
+
+      expect(res.body).toEqual({
+        accessToken: res.body.accessToken,
+        user: {
+          ...user,
+          id: res.body.user.id,
+          createdAt: res.body.user.createdAt,
+        },
+      });
+    });
+  });
+
+  describe('/auth/forgot-password POST', () => {
+    it('should return message for correct email', async () => {
+      return await request(app.getHttpServer())
+        .post('/auth/forgot-password')
+        .send({ email: user.email })
+        .expect(200)
+        .expect({
+          message: 'Check your email to change password',
+        });
+    });
+
+    it('should return message for incorrect email', async () => {
+      return await request(app.getHttpServer())
+        .post('/auth/forgot-password')
+        .send({ email: 'incorrect@email.com' })
+        .expect(200)
+        .expect({
+          message: 'Check your email to change password',
+        });
+    });
+  });
+
+  describe('/auth/change-password POST', () => {
+    it('should change password and return user data with token', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/auth/change-password')
+        .set('Authorization', 'Bearer ' + accessToken)
+        .send({ password: newPassword, repeatPassword: newPassword })
+        .expect(200);
+
+      expect(res.body).toEqual({
+        accessToken: res.body.accessToken,
+        user: {
+          ...user,
+          id: res.body.user.id,
+          createdAt: res.body.user.createdAt,
+        },
+      });
+    });
+
+    it('should login after change password', async () => {
+      return await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: user.email, password: newPassword })
+        .expect(200);
+    });
+
+    it('should not permit change without auth token', async () => {
+      return await request(app.getHttpServer())
+        .post('/auth/change-password')
+        .send({ email: user.email })
+        .expect(401)
+        .expect({
+          statusCode: 401,
+          message: 'Unauthorized',
+        });
+    });
+
+    it('should return error about incorrect data', async () => {
+      return await request(app.getHttpServer())
+        .post('/auth/change-password')
+        .set('Authorization', 'Bearer ' + accessToken)
+        .send({ password: newPassword, repeatPassword: 'incorrect' })
+        .expect(400)
+        .expect({
+          statusCode: 400,
+          message: 'Incorrect repeatPassword',
+          error: 'Bad Request',
         });
     });
   });
